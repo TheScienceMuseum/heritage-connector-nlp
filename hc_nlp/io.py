87280728942e140c5aeff5c25c3afcdaaedbf9eb
Kalyan Dutia <kalyan.dutia@gmail.com>
2020-11-27 14:55:15 +0000

    ource /Users/kalyan/.local/share/virtualenvs/heritage-connector-nlp-u52fn-qD/bin/activate evert "freedom in selecting relative path"
    This reverts commit 5cec5894e1f152829068bbcb152821a89f2cc98a.

--- hc_nlp/io.py
+++ hc_nlp/io.py
@@ -5,40 +5,42 @@ from typing import Tuple, List
 from hc_nlp.spacy_helpers import correct_entity_boundaries
 
 
-def load_raw_labelstudio_results(results_path: str) -> dict:
+def load_raw_labelstudio_results(results_set: str) -> dict:
     """
-    Load results from Label Studio export (in the folder labelling/export/<date>.zip by default).
+    Load results specified by their date in the filename (the name of the zip file in 
+    labelling/export/NAME.zip without '.zip').
 
     Args:
-        results_path (str): path to results
+        results_set (str)
 
     Returns:
         dict: see Label Studio docs
     """
 
-    if not results_path.endswith(".zip"):
+    file_name = os.path.join(
+        os.path.dirname(__file__), f"../labelling/export/{results_set}.zip"
+    )
+
+    if not os.path.exists(file_name):
         raise ValueError(
-            "`results_path` must point to a .zip file exported by Label Studio, or equivalent."
+            f"Results with date {results_set} (at {file_name}) do not exist."
         )
 
-    if not os.path.exists(results_path):
-        raise ValueError(f"Results at {results_path} do not exist.")
-
-    with ZipFile(results_path, "r") as z:
+    with ZipFile(file_name, "r") as z:
         data = json.loads(z.read("result.json"))
 
     return data
 
 
 def load_text_and_annotations_from_labelstudio(
-    results_path: str, spacy_model=None, adjust_entity_boundaries=True
+    results_set: str, spacy_model=None, adjust_entity_boundaries=True
 ) -> List[Tuple[str, List[Tuple[int, int, str]]]]:
     """
     Load text and completed annotations from Label Studio, in the form [("my text", [(1, 3, "ORG"), (4, 10, "PERSON")]), ...].
     `spacy_model` is required for tokenization if `adjust_entity_boundaries` is set to True.
 
     Args:
-        results_path (str): path to results
+        results_set (str): name of the zip file in labelling/export/NAME.zip without '.zip'
         spacy_model: model used for tokenization
         adjust_entity_boundaries: whether to correct entity boundaries according to token boundaries found by the tokenizer.
             If true, will adjust the start and end of the location of each entity to the closest true start or end.
@@ -47,7 +49,7 @@ def load_text_and_annotations_from_labelstudio(
         List[str, List[Tuple[int, int, str]]]: text, annotations
     """
 
-    docs = load_raw_labelstudio_results(results_path)
+    docs = load_raw_labelstudio_results(results_set)
     results = []
 
     for doc in docs:
