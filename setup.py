f6000a4428a8593f823b975c02dd25e0394f78b0
Kalyan Dutia <kalyan.dutia@gmail.com>
2020-12-09 11:45:04 +0000

    import from label studio now uses path

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="hc-nlp",
-    version="0.1.0",
+    version="0.2.0",
     author="Science Museum Group",
     description="Heritage Connector NLP",
     long_description=long_description,
