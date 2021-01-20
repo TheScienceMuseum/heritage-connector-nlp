c4adb8afc7e6618f86f2aecd9ee6d2347bd57ec3
Kalyan Dutia <kalyan.dutia@gmail.com>
2021-01-20 16:06:35 +0000

    bump spacy-nightly spacy-transformers versions

--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
-    install_requires=["spacy-nightly==3.0.0rc2", "spacy-transformers==1.0.0rc0"],
+    install_requires=["spacy-nightly==3.0.0rc3", "spacy-transformers==1.0.0rc2"],
     packages=["hc_nlp"],
 )
